a7fb989bc9044da6c43d3946ecd64a456143067f
adlered <1101635162@qq.com>
2021-11-27 17:12:48 +0800

    :art: Telnet功能调试

--- src/main/java/org/pwl/telnet/TelnetUtil.java
+++ src/main/java/org/pwl/telnet/TelnetUtil.java
@@ -1,5 +1,6 @@
 package org.pwl.telnet;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class TelnetUtil implements Runnable {
@@ -13,7 +14,7 @@ public class TelnetUtil implements Runnable {
         RunningConfig runningConfig = new RunningConfig();
         Trans.send(socket, runningConfig.encode, "Welcome to Telnet Landlord! What is your system?\r\n");
         Trans.send(socket, runningConfig.encode, "1: Windows (GB2312)\r\n");
-        Trans.send(socket, runningConfig.encode, "2: Linux/macOS (UTF-8)\r\n");
+        Trans.send(socket, runningConfig.encode, "2: Linux/macOS (UTF-8, Default)\r\n");
         String quote = Trans.input(socket, runningConfig.encode);
         if (quote.length() > 1) {
             runningConfig.encode = "UTF-8";
@@ -21,7 +22,32 @@ public class TelnetUtil implements Runnable {
             runningConfig.encode = "GB2312";
         } else if (quote.equals("2")) {
             runningConfig.encode = "UTF-8";
+        } else {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
         }
-
+        Trans.send(socket, runningConfig.encode, "================================\r\n");
+        Trans.send(socket, runningConfig.encode, "=    鱼油，欢迎来到斗地主！\r\n");
+        Trans.send(socket, runningConfig.encode, "================================\r\n");
+        Trans.send(socket, runningConfig.encode, "请选择登录方式：\r\n");
+        Trans.send(socket, runningConfig.encode, "1. 摸鱼派 (https://pwl.icu) 社区账号登录\r\n");
+        String login = Trans.input(socket, runningConfig.encode);
+        if (login.equals("1")) {
+            Trans.send(socket, runningConfig.encode, "请输入摸鱼派社区用户名\r\n");
+            String username = Trans.input(socket, runningConfig.encode);
+            Trans.send(socket, runningConfig.encode, "请输入摸鱼派社区密码\r\n");
+            String password = Trans.input(socket, runningConfig.encode);
+        } else {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
     }
 }
